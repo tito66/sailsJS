78958a52fb25c09088450ef89ac8a9051a18f894
tito66 <caguirremartinez868@gmail.net>
2017-01-22 17:57:28 -0600

    lolis

---
+++ api/controllers/CustomerController.js
@@ -0,0 +1,11 @@
+/**
+ * CustomerController
+ *
+ * @description :: Server-side logic for managing Customers
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ config/passport.js
@@ -0,0 +1,93 @@
+/**
+ * Passport configuration file where you should configure strategies
+ */
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var JwtStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;  
+
+//var expiresIn = 24;
+//var expiresIn = 60 * 24;
+var expiresIn= '1h';
+
+// var expiresIn = 60*60*24;
+
+var SECRET = process.env.tokenSecret || "4ukI0uIVnB3iI1yxj646fVXSE3ZVk4doZgz6fTbNg7jO41EAtl20J5F7Trtwe7OM";
+var ALGORITHM = "HS256";
+var ISSUER = "nozus.com";
+var AUDIENCE = "nozus.com";
+ 
+/**
+ * Configuration object for local strategy
+ */
+var LOCAL_STRATEGY_CONFIG = {
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: false
+};
+ 
+/**
+ * Configuration object for JWT strategy
+ */
+var JWT_STRATEGY_CONFIG = {
+  jwtFromRequest: ExtractJwt.fromAuthHeader(),
+  secretOrKey: SECRET,
+  issuer : ISSUER,
+  audience: AUDIENCE,
+  passReqToCallback: false
+};
+ 
+ /*
+var JWT_STRATEGY_CONFIG = {
+  secretOrKey: SECRET,
+  issuer : ISSUER,
+  audience: AUDIENCE,
+  passReqToCallback: false
+};
+ */
+
+/**
+ * Triggers when user authenticates via local strategy
+ */
+function _onLocalStrategyAuth(email, password, next) {
+  User.findOne({email: email})
+    .exec(function (error, user) {
+      if (error) return next(error, false, {});
+ 
+      if (!user) return next(null, false, {
+        code: 'E_USER_NOT_FOUND',
+        message: email + ' is not found'
+      });
+ 
+      // TODO: replace with new cipher service type
+      if (!CipherService.comparePassword(password, user))
+        return next(null, false, {
+          code: 'E_WRONG_PASSWORD',
+          message: 'Password is wrong'
+        });
+ 
+      return next(null, user, {});
+    });
+}
+ 
+/**
+ * Triggers when user authenticates via JWT strategy
+ */
+function _onJwtStrategyAuth(payload, next) {
+  var user = payload.user;
+  return next(null, user, {});
+}
+ 
+passport.use(
+  new LocalStrategy(LOCAL_STRATEGY_CONFIG, _onLocalStrategyAuth));
+
+passport.use(
+  new JwtStrategy(JWT_STRATEGY_CONFIG, _onJwtStrategyAuth)); 
+ 
+module.exports.jwtSettings = {
+  expiresIn: expiresIn,
+  secret: SECRET,
+  algorithm : ALGORITHM,
+  issuer : ISSUER,
+  audience : AUDIENCE
+};
